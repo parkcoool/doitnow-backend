8da7e89698046fa99386cc45daace052e5683d10
parkcoool <yickle1345@naver.com>
2024-02-24 18:38:19 +0900

    Feat: Add `cors` middleware

--- app.ts
+++ app.ts
@@ -1,14 +1,25 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 import apiRouter from "./routes/api";
+
+dotenv.config();
 
 const app = express();
 const port = 80;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api", apiRouter);
 
 app.listen(port, () => {
+  console.log(process.env.CLIENT_URL);
   console.log(`App is listening on port ${port}`);
 });
