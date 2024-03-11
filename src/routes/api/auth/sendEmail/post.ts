15c80760187caf5a544e8a0f9321cb50de135003
parkcoool <yickle1345@naver.com>
2024-03-11 12:43:49 +0900

    Feat: Create `addEmailVerifyCode` model

--- src/routes/api/auth/sendEmail/post.ts
+++ src/routes/api/auth/sendEmail/post.ts
@@ -1,8 +1,10 @@
 import sendEmailRouter from "./";
 
 import StatusCode from "constant/statusCode";
+import addEmailVerifyCode from "model/emailVerify/addEmailVerifyCode";
 
 import type { APIResponse } from "api";
+import type { QueryError } from "mysql2";
 
 interface ReqeustBody {
   email: string;
@@ -22,18 +24,49 @@ sendEmailRouter.post<"/", {}, APIResponse<ResponseBody | null>, ReqeustBody>("/"
     });
   }
 
+  // 인증 코드 생성
+  const code = Math.random().toString(36).substring(2, 6);
+
+  // 이메일 주소 정제
+  const email = req.body.email.trim();
+
+  // 인증 코드 만료 시간 설정
   const expiresAt = new Date();
   expiresAt.setMinutes(expiresAt.getMinutes() + 5);
-  // expiresAt.setSeconds(expiresAt.getSeconds() + 5);
 
-  // 이메일 주소를 확인하여 인증 코드를 발송하는 로직
-  // ...
+  // 클라이언트 IP 주소 추출
+  const ipAddr = req.ip;
+
+  // 인증 코드를 데이터베이스에 저장
+  try {
+    const queryResult = await addEmailVerifyCode(code, email, expiresAt, ipAddr);
+    if (queryResult.affectedRows === 0) throw new Error();
+  } catch (e) {
+    res.status(200).send({
+      code: StatusCode.SERVER_ERROR,
+      message: "인증 코드를 등록하는 도중 오류가 발생했습니다.",
+      result: null,
+    });
+    return;
+  }
+
+  // 인증 코드를 이메일로 발송
+  try {
+    // ...
+  } catch (e) {
+    res.status(200).send({
+      code: StatusCode.SERVER_ERROR,
+      message: "인증 코드를 발송하는 도중 오류가 발생했습니다.",
+      result: null,
+    });
+    return;
+  }
 
   res.status(200).send({
     code: StatusCode.SUCCESS,
     message: "인증 코드를 성공적으로 발송하였습니다.",
     result: {
-      email: req.body.email,
+      email: email,
       expiresAt: expiresAt.toISOString(),
     },
   });
--- src/util/getSaltedHash.ts
+++ src/util/getSaltedHash.ts
@@ -1,5 +1,5 @@
 import CryptoJS from "crypto-js";
 
-export default function getSaltedHash(password: string, salt: string) {
-  return CryptoJS.SHA512(`${password}${salt}`).toString();
+export default function getSaltedHash(password: string, salt?: string) {
+  return CryptoJS.SHA512(`${password}${salt ?? ""}`).toString();
 }
