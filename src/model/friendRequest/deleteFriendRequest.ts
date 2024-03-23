94176fa96b231089725606966375b9ac81d5cfc4
parkcoool <yickle1345@naver.com>
2024-03-23 14:31:11 +0900

    Feat: Add `id` prop to `deleteFriendRequest` model

--- src/model/friendRequest/deleteFriendRequest.ts
+++ src/model/friendRequest/deleteFriendRequest.ts
@@ -1,17 +1,24 @@
 import db from "model";
 
-import type { ResultSetHeader } from "mysql2";
+import type { FieldPacket, ResultSetHeader } from "mysql2";
 
 interface DeleteFriendRequestProps {
-  from: number;
-  to: number;
+  from?: number;
+  to?: number;
+  id?: number;
 }
 
-export default async function deleteFriendRequest({ from, to }: DeleteFriendRequestProps) {
-  const queryResult = await db.query<ResultSetHeader>("DELETE FROM friend_request WHERE `from` = ? AND `to` = ?", [
-    from,
-    to,
-  ]);
+export default async function deleteFriendRequest({ from, to, id }: DeleteFriendRequestProps) {
+  let queryResult: [ResultSetHeader, FieldPacket[]];
+
+  if (from !== undefined && to !== undefined && id !== undefined)
+    queryResult = await db.query("DELETE FROM friendRequest WHERE `from` = ? AND `to` = ? AND id = ?", [from, to, id]);
+  else if (from !== undefined && to !== undefined)
+    queryResult = await db.query("DELETE FROM friendRequest WHERE `from` = ? AND `to` = ?", [from, to]);
+  else if (id !== undefined) queryResult = await db.query("DELETE FROM friendRequest WHERE id = ?", [id]);
+  else if (from !== undefined) queryResult = await db.query("DELETE FROM friendRequest WHERE `from` = ?", [from]);
+  else if (to !== undefined) queryResult = await db.query("DELETE FROM friendRequest WHERE `to` = ?", [to]);
+  else throw new Error("At least one of from, to, id should be provided.");
 
   return queryResult;
 }
