51768ccf62d1a60872c7aaec69a33d7da223afd7
parkcoool <yickle1345@naver.com>
2024-03-25 16:53:47 +0900

    Feat: Remove key and cert files

--- app.ts
+++ app.ts
@@ -2,26 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import https from "https";
-import fs from "fs";
+// import fs from "fs";
 
 import apiRouter from "./src/routes/api";
 
 dotenv.config();
 
 const options = {
-  key: fs.readFileSync("./src/config/key.pem"),
-  cert: fs.readFileSync("./src/config/cert.pem"),
+  // key: fs.readFileSync("./src/config/key.pem"),
+  // cert: fs.readFileSync("./src/config/cert.pem"),
 };
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api", apiRouter);
 
